77c88c6aece2766e6cac4b6d93ffed97bcac4008
Myles Kaufman <myleskaufman.mk@gmail.com>
2025-02-21 18:42:32 -0600

    Add a somewhat function sidebar for app navigation. Styling still needs work.

---
+++ public/js/main.js
@@ -0,0 +1,6 @@
+const toggleButton = document.getElementById("sidebar-toggle")
+const sidebar = document.getElementById("sidebar")
+
+function toggleSidebar(){
+    sidebar.classList.toggle("close")
+}
--- server.js
+++ server.js
@@ -4,8 +4,10 @@ const http = require('http');
 const server = http.createServer(app);
 const PORT = process.env.PORT || 3000;
 
-
+//Serve html, css and js files
 app.use(express.static('public/html'));
+app.use('/css', express.static('public/css'));
+app.use('/js', express.static('public/js'));
 
 
 server.listen(PORT, () => {
