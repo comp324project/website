dc6aa6b4ab7b55e556175f634caee0f2481dc02b
areuschel <104516533+areuschel@users.noreply.github.com>
2025-03-22 17:05:22 -0500

    added user resume info schema to User.js

--- public/models/User.js
+++ public/models/User.js
@@ -5,7 +5,20 @@ const userSchema = new mongoose.Schema({
   username: { type: String, required: true, unique: true },
   email: { type: String, required: true, unique: true },
   password: { type: String, required: true },
+  // masterResume containing arrays to store user input -- adrian added 03/22
+  // not required bc input on separate form
+  masterResume: { 
+    skills: [{ type: String}],
+    experience: [{type: String}],
+    projects: [{type: String}],
+    research: [{type: String}],
+    volunteering: [{type: String}],
+    education: [{type: String}],
+    references: [{type: String}]
+},
 }, { timestamps: true }); //This enables Mongo-side creation and update timestamps -> SUPER USEFUL);
+
+// comparePassword code: https://www.mongodb.com/blog/post/password-authentication-with-mongoose-part-1
 
 // Hash the password before saving the user
 userSchema.pre('save', async function (next) {
