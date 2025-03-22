125e5975282a28ed1839ba47db34b24c126f324a
areuschel <104516533+areuschel@users.noreply.github.com>
2025-03-22 17:34:56 -0500

    added schema for saving resume output

---
+++ public/models/resumeSchema.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose');
+const { Schema } = mongoose;
+
+// Schema
+const resumeSchema = new Schema({
+  user: { type: Schema.Types.ObjectId, ref: 'User', required: true }, // FK relationship to user
+  job: {type: Schema.Types.ObjectId, ref: 'Job', required: true}, // FK relationship to job 
+  // name of the output or resume
+  template_name: { type: String, required: true },
+  content: {type: String, required: true},
+  created_at: {type: Date, default: Date.now}
+});
+
+const Resume = mongoose.model('Resume', resumeSchema);
+
+module.exports = Resume;
