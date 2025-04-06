671d8bcbcf74cd4f63f4451b957911f9404b62d3
Myles Kaufman <myleskaufman.mk@gmail.com>
2025-04-06 15:46:33 -0500

    Minor Route Fix

--- public/routes/index.js
+++ public/routes/index.js
@@ -6,7 +6,6 @@ const router = express.Router();
 const brightDataRoutes = require("./brightData");
 const authRoutes = require("./auth");
 const userRoutes = require("./user");
-const jobRoutes = require("./jobRoutes"); // added 04/04 for job data
 
 //Import index controller
 const indexController=require("../controllers/index");
@@ -15,7 +14,6 @@ const indexController=require("../controllers/index");
 router.use("/api/brightdata/", brightDataRoutes);
 router.use("/auth",authRoutes);
 router.use('/db', userRoutes)
-router.use("/api/jobs", jobRoutes);
 
 //Handles webpage entry routing
 router.get("/",indexController.index)
