56eee315e3ef319044538856fcba99608e7d86c4
areuschel <104516533+areuschel@users.noreply.github.com>
2025-04-07 15:45:13 -0500

    links for js models

---
+++ public/data/BERT.js
@@ -0,0 +1,7 @@
+// To be added to an html:
+// <script src="https://cdn.jsdelivr.net/npm/@tensorflow-models/universal-sentence-encoder"></script>
+
+
+// code source: https://medium.com/@j622amilah/tensorflow-js-example-models-91094bd74a90
+// git source: https://github.com/tensorflow/tfjs-models/tree/master/universal-sentence-encoder
+
