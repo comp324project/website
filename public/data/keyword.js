1fc5b88ba3b4022addb9f5948cee8b7720f44bf4
areuschel <104516533+areuschel@users.noreply.github.com>
2025-04-11 09:59:17 -0500

    merge error fix

--- public/data/keyword.js
+++ public/data/keyword.js
@@ -45,11 +45,7 @@ const testKeywordExtractor = async () => {
       const filteredSkills = keywords.filter(word => knownSkills.includes(word));
 
       console.log(`\nJob Title: ${job.title}`);
-<<<<<<< HEAD
       console.log(`Extracted Filtered Keywords:`, filteredSkills);
-=======
-      console.log(`Extracted filtered Keywords:`, filteredSkills);
->>>>>>> adrian-r
     }
     
   } catch (error) {
