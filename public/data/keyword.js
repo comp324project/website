a182f44007c5312faa908d3c917bffcda5d5b187
areuschel <104516533+areuschel@users.noreply.github.com>
2025-04-11 09:52:37 -0500

    filtering test

--- public/data/keyword.js
+++ public/data/keyword.js
@@ -2,11 +2,27 @@
 // Resource from Dr. Hayward:
 // https://www.npmjs.com/package/keyword-extractor
 
+/*
+TO DO:
+- filter for technical skills (soft skills necessary to ALL jobs.. maybe drop this)
+    - need new file for skills list (looking online for existing or need to manually create..)
+- new file for scoring algorithm (simple weighted and normalized?)
+*/
+
+
+
+
 // keyword extractor test for historical data
 require('dotenv').config();
 const { connectDB, disconnectDB } = require('../../config/mongo');
 const Job = require('../models/jobSchema');
 const keyword_extractor = require('keyword-extractor');
+
+// update later with new file! this is used for testing right now
+const knownSkills = [
+    'javascript', 'python', 'react', 'node', 'aws', 'docker', 
+    'frontend', 'front-end', 'front end', 'RStudio'
+  ];
 
 // connect to db, fetch jobs, loop and get keywords, log result to console
 const testKeywordExtractor = async () => {
@@ -26,9 +42,10 @@ const testKeywordExtractor = async () => {
         return_changed_case: true,
         remove_duplicates: false
       });
+      const filteredSkills = keywords.filter(word => knownSkills.includes(word));
 
       console.log(`\nJob Title: ${job.title}`);
-      console.log(`Extracted Keywords:`, keywords);
+      console.log(`Extracted Filtered Keywords:`, filteredSkills);
     }
     
   } catch (error) {
